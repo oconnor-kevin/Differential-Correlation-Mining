263e184a7a8a08e5b190f9de41224eb16b1447f7
oconnor-kevin <oconnor.kevin.ant@gmail.com>
2018-12-02 10:54:35 -0500

    Updated stdize.R

--- DCM/R/stdize.R
+++ DCM/R/stdize.R
@@ -1,14 +1,17 @@
-# Standardize matrix of data, by rows
-
-stdize <-
-function(Mat){
-	
+#' stdize
+#'
+#' Standardize matrix to have mean=0 and sum of squares=1, by rows.
+#'
+#' @param mat Data matrix to be row-standardized.
+#'
+#' @return Row-standardized matrix.
+#'
+#' @export
+stdize <- function(Mat){
 	# Subtract mean of each row
-	Mat = Mat - rowMeans(Mat)
-	
+	Mat <- Mat - rowMeans(Mat)
 	# Find sd of each row
-	sds = sqrt(rowSums(Mat*Mat))
-	
-	# Return stdized
+	sds <- sqrt(rowSums(Mat*Mat))
+
 	return(Mat/sds)
 }
