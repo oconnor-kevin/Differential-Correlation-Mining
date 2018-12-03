ec70aa97532cd1ef871bd55b11b804ef5ee0dbdc
oconnor-kevin <oconnor.kevin.ant@gmail.com>
2018-12-02 21:29:03 -0500

    quantNorm + fixed typo in sanitize_DCM

--- DCM/R/quantNorm.R
+++ DCM/R/quantNorm.R
@@ -1,15 +1,19 @@
-# Quantile normalize a vector of data
-
-quantNorm <-
-function(Mat){
-	
+#' quantNorm
+#' 
+#' Quantile normalize rows of a given matrix.
+#' 
+#' @param Mat Matrix to be quantile normalized.
+#' 
+#' @return Matrix with rows quantile normalized.
+#' 
+#' @export
+quantNorm <- function(Mat){
 	# Find row length
-	n = dim(Mat)[2]
-
+	n <- dim(Mat)[2]
 	# Rank each row
-	ranks = t(apply(Mat, 1, rank))
-	#Mat = qnorm(ranks/(n+1))
-	Mat = matrix(qnorm(ranks/(n+1)), ncol = n)
+	ranks <- t(apply(Mat, 1, rank))
+  # Store quantile-normalized data.
+	Mat <- matrix(qnorm(ranks/(n+1)), ncol = n)
 	
 	return(Mat)
 }
